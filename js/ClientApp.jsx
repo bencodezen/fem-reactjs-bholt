e46dc78a9b703bdb669ae8760c0f8da8c202aa1c
Ben Hong <bencodezen@gmail.com>
2017-07-02 13:57:47 -0400

    style: Format with Prettier on save

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,24 +1,23 @@
-const ce = React.createElement
+const ce = React.createElement;
 
 const myTitle = function(props) {
-	return (
-		ce('div', null,
-			ce('h1', { style: { color: props.color } }, props.title)
-		)
-	)
-}
+	return ce(
+		"div",
+		null,
+		ce("h1", { style: { color: props.color } }, props.title)
+	);
+};
 
 const myFirstComponent = function() {
-	return ce('div', null,
-		ce(myTitle, { title: 'Game of Thrones', color: 'LemonChiffon' }),
-		ce(myTitle, { title: 'Westworld', color: 'Crimson' }),
-		ce(myTitle, { title: 'Silicon Valley', color: 'PapayaWhip' }),
-		ce(myTitle, { title: 'Red Rising', color: 'BurlyWood' }),
-		ce(myTitle, { title: 'Sabriel', color: 'PeachPuff' })
-	)
-}
+	return ce(
+		"div",
+		null,
+		ce(myTitle, { title: "Game of Thrones", color: "LemonChiffon" }),
+		ce(myTitle, { title: "Westworld", color: "Crimson" }),
+		ce(myTitle, { title: "Silicon Valley", color: "PapayaWhip" }),
+		ce(myTitle, { title: "Red Rising", color: "BurlyWood" }),
+		ce(myTitle, { title: "Sabriel", color: "PeachPuff" })
+	);
+};
 
-ReactDOM.render(
-	ce(myFirstComponent),
-	document.getElementById('app')
-)
+ReactDOM.render(ce(myFirstComponent), document.getElementById("app"));
