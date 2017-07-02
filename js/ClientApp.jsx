5f6fffbe18d6d0e58279fa7177b6f2a1dae19885
Ben Hong <bencodezen@gmail.com>
2017-07-02 14:27:18 -0400

    config: Use Webpack to bundle code

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,23 +1,26 @@
+import React from 'react';
+import { render } from 'react-dom';
+
 const ce = React.createElement;
 
 const myTitle = function(props) {
 	return ce(
-		"div",
+		'div',
 		null,
-		ce("h1", { style: { color: props.color } }, props.title)
+		ce('h1', { style: { color: props.color } }, props.title)
 	);
 };
 
 const myFirstComponent = function() {
 	return ce(
-		"div",
+		'div',
 		null,
-		ce(myTitle, { title: "Game of Thrones", color: "LemonChiffon" }),
-		ce(myTitle, { title: "Westworld", color: "Crimson" }),
-		ce(myTitle, { title: "Silicon Valley", color: "PapayaWhip" }),
-		ce(myTitle, { title: "Red Rising", color: "BurlyWood" }),
-		ce(myTitle, { title: "Sabriel", color: "PeachPuff" })
+		ce(myTitle, { title: 'Game of Thrones', color: 'LemonChiffon' }),
+		ce(myTitle, { title: 'Westworld', color: 'Crimson' }),
+		ce(myTitle, { title: 'Silicon Valley', color: 'PapayaWhip' }),
+		ce(myTitle, { title: 'Red Rising', color: 'BurlyWood' }),
+		ce(myTitle, { title: 'Sabriel', color: 'PeachPuff' })
 	);
 };
 
-ReactDOM.render(ce(myFirstComponent), document.getElementById("app"));
+render(ce(myFirstComponent), document.getElementById('app'));
