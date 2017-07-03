8e2a89130a2e967ed26b7f9c578960bbeca496ea
Ben Hong <bencodezen@gmail.com>
2017-07-03 11:08:37 -0400

    feat: Add basic ShowCard component

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
+import ShowCard from './ShowCard';
 import preload from '../data';
 
 const Search = () =>
 	<div className="search">
-		<pre>
-			<code>
-				{JSON.stringify(preload, null, 4)}
-			</code>
-		</pre>
+		{preload.shows.map(show => <ShowCard show={show} />)}
 	</div>;
 
 export default Search;
