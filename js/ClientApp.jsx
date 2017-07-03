ab641c031224737a753357c4259b89f6e456086f
Ben Hong <bencodezen@gmail.com>
2017-07-02 21:42:52 -0400

    config: Allow for browser routing

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { HashRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Landing from './Landing';
 import Search from './Search';
 
 const App = () =>
-	<HashRouter>
+	<BrowserRouter>
 		<div className="app">
 			<Route exact path="/" component={Landing} />
 			<Route exact path="/search" component={Search} />
 		</div>
-	</HashRouter>;
+	</BrowserRouter>;
 
 render(<App />, document.getElementById('app'));
