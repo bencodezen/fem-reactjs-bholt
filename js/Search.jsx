508e6edc52a0ad21f86ebf8342b13aea2162d081
Ben Hong <bencodezen@gmail.com>
2017-07-03 22:10:47 -0400

    feat: Add header for Search page

--- js/Search.jsx
+++ js/Search.jsx
@@ -4,6 +4,10 @@ import preload from '../data';
 
 const Search = () =>
 	<div className="search">
+		<header>
+			<h1>React Video Player</h1>
+			<input type="text" placeholder="Search" />
+		</header>
 		<div>
 			{preload.shows.map(show =>
 				<ShowCard key={show.imdbID} show={show} />
