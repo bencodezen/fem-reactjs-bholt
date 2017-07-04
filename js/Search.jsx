a90966c97f73d33b07a5b996504d058cf3c73b72
Ben Hong <bencodezen@gmail.com>
2017-07-03 22:20:36 -0400

    feat: Add state to the search input

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ShowCard from './ShowCard';
 import preload from '../data';
 
-const Search = () =>
-	<div className="search">
-		<header>
-			<h1>React Video Player</h1>
-			<input type="text" placeholder="Search" />
-		</header>
-		<div>
-			{preload.shows.map(show =>
-				<ShowCard key={show.imdbID} show={show} />
-			)}
-		</div>
-	</div>;
+class Search extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			searchTerm: 'this is some sort of debug statement'
+		};
+
+		this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
+	}
+
+	handleSearchTermChange(event) {
+		this.setState({ searchTerm: event.target.value });
+	}
+
+	render() {
+		return (
+			<div className="search">
+				<header>
+					<h1>React Video Player</h1>
+					<input
+						type="text"
+						placeholder="Search"
+						value={this.state.searchTerm}
+						onChange={this.handleSearchTermChange}
+					/>
+				</header>
+				<div>
+					{preload.shows.map(show =>
+						<ShowCard key={show.imdbID} show={show} />
+					)}
+				</div>
+			</div>
+		);
+	}
+}
 
 export default Search;
